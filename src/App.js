2d8454fa9b2167a6dadc6102d102c9c141513880
Boris Lebedev <38631984+lebedevmoscow@users.noreply.github.com>
2020-11-16 03:39:00 +0500

    Add comments (since they weren't there)

--- src/App.js
+++ src/App.js
@@ -1,85 +1,114 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react'
 import { useStopwatch } from 'react-timer-hook'
 import { useSpeechSynthesis } from 'react-speech-kit'
-import './App.css';
+import './App.css'
 
 export default function App() {
-  
-  const { seconds, isRunning, start, reset } = useStopwatch();
-  const { speak, supported } = useSpeechSynthesis();
+	// Simple timer
+	const { seconds, isRunning, start, reset } = useStopwatch()
 
-  const [data, setData] = useState([
-    {timer: 2, name: 'Write here '}
-  ])
+	// Speach API
+	const { speak, supported } = useSpeechSynthesis()
 
-  const doReset = useCallback(() => reset(), [])
-  const doSpeak = useCallback((text) => speak({text: text}), [])
+	// Initial State for render
+	const [data, setData] = useState([{ timer: 2, name: 'Write here ' }])
 
-  useEffect(() => {
+	// Reset the timer
+	const doReset = useCallback(() => reset(), [])
 
-    const result = data.find((item) => {
-      return item.timer === seconds
-    })
+	// Start speaking
+	const doSpeak = useCallback((text) => speak({ text: text }), [])
 
-    if (result) {
-      doSpeak(result.name)
-    }
+	useEffect(() => {
+		// Compare the present count of seconds and the each timer in queue
+		const result = data.find((item) => {
+			return item.timer === seconds
+		})
 
-    if (seconds > data[data.length - 1].timer) {
-      doReset()
-    }
+		// If time is has come
+		if (result) {
+			doSpeak(result.name)
+		}
 
-  }, [data, seconds, doSpeak, doReset])
+		// If timer has done
+		if (seconds > data[data.length - 1].timer) {
+			doReset()
+		}
+	}, [data, seconds, doSpeak, doReset])
 
-  const onChangeHandler = (e, index) => {
-    const dataClone = data.concat()
-    const element = dataClone.find((item, idx) => {
-      return idx === index
-    })
+	// Push new task to queue on speaking
+	const onChangeHandler = (e, index) => {
+		const dataClone = data.concat()
+		const element = dataClone.find((item, idx) => {
+			return idx === index
+		})
 
-    if (e.target.type === 'text') element.name = e.target.value
-    if (e.target.type === 'number') element.timer = ++e.target.value
+		// Type cast
+		if (e.target.type === 'text') element.name = e.target.value
+		if (e.target.type === 'number') element.timer = ++e.target.value
 
-    dataClone[index] = element
-    setData(dataClone)
-  }
+		dataClone[index] = element
+		setData(dataClone)
+	}
 
-  const addNewTimer = () => {
-    const newData = [...data, {timer: 10, name: 'Enter here your message'}]
-    setData(newData)
-  }
+	// Add new task to queue on speaking
+	const addNewTimer = () => {
+		const newData = [
+			...data,
+			{ timer: 10, name: 'Enter here your message' },
+		]
+		setData(newData)
+	}
 
-  return (
-    <div className="app">
-      <h2>Talk the Talk</h2>
+	return (
+		<div className='app'>
+			<h2>Talk the Talk</h2>
 
-      {!supported && <h1>Sorry, your browser is not supported this application.</h1>}
+			{/* If user browser does not support speech api */}
+			{!supported && (
+				<h1>Sorry, your browser is not supported this application.</h1>
+			)}
 
-      <div className="timers">
-        {/* timers go here */}
-          {
-            data.map((element, index) => {
-              return(
-                <form key={index} className="timer">
-                  <input onChange={(e) => onChangeHandler(e, index)} type="number" defaultValue={element.timer}/>
-                  <input onChange={(e) => onChangeHandler(e, index)} type="text" defaultValue={element.name}/>
-                </form>
-              )
-            })
-          }
+			<div className='timers'>
+				{/* timers go here */}
+				{data.map((element, index) => {
+					return (
+						<form key={index} className='timer'>
+							<input
+								onChange={(e) => onChangeHandler(e, index)}
+								type='number'
+								defaultValue={element.timer}
+							/>
+							<input
+								onChange={(e) => onChangeHandler(e, index)}
+								type='text'
+								defaultValue={element.name}
+							/>
+						</form>
+					)
+				})}
 
+				<button onClick={() => addNewTimer()} className='add-button'>
+					Add
+				</button>
+			</div>
 
-        <button onClick={() => addNewTimer()} className="add-button">Add</button>
-      </div>
+			{/* seconds */}
+			<h2>{seconds}</h2>
 
-      {/* seconds */}
-      <h2>{seconds}</h2>
-
-      {/* buttons */}
-      <div className="buttons">
-        {!isRunning && <button onClick={() => start()} className="start-button">Start</button> }
-        {isRunning && <button onClick={() => reset()} className="stop-button">Stop</button> }
-      </div>
-    </div>
-  );
+			{/* buttons */}
+			<div className='buttons'>
+				{!isRunning && (
+					<button onClick={() => start()} className='start-button'>
+						Start
+					</button>
+				)}
+				{isRunning && (
+					<button onClick={() => reset()} className='stop-button'>
+						Stop
+					</button>
+				)}
+			</div>
+		</div>
+	)
 }
